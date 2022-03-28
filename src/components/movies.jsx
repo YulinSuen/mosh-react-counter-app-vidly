998b64357c96ad906437c1b420f75ba7dd3d6ba3
Yulin <1710285545@qq.com>
2022-03-28 16:14:59 +0800

    19-Conditional Rendering & 18-Deleted

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,6 +3,12 @@ import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = { movies: getMovies() };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
   render() {
     //   short cuts to create table
     //  table.table>thead>tr>th*4
@@ -14,15 +20,24 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
