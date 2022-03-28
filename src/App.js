80f3eb424a0d24c297ddd1917e1bc4f99c6ef8ac
Yulin <1710285545@qq.com>
2022-03-28 16:05:29 +0800

    18-Deleting a Movie & 17-Built Movie Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Movies from "./components/movies";
 import "./App.css";
 class App extends Component {
   render() {
     return (
-      <main className="Container">
-        <h1>Hello World</h1>
+      <main className="container">
+        <Movies />
       </main>
     );
   }
