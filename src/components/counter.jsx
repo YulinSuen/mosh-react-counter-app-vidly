1c23f5e057151b6ff2f7773ad9da1d586666ea23
Yulin <1710285545@qq.com>
2022-03-28 14:57:31 +0800

    11-One way to bind Counter Object to Handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,15 @@ class Counter extends Component {
     count: 0,
   };
 
+  //One way to bind Counter Object to Handler
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  // 在对象的方法种，this指代对象, 在普通方法中，this指代windows, in strict mode, this is undefined
   handleIncrement() {
-    console.log("Increment Clicked");
+    console.log("Increment Clicked", this);
   }
 
   render() {
