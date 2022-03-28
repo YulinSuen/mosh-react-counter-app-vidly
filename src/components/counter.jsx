367880a53648f68d08eb37128b445d6a4972e3c7
Yulin <1710285545@qq.com>
2022-03-28 15:06:00 +0800

    13-What Happens When State Changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,16 +4,9 @@ class Counter extends Component {
   state = {
     count: 0,
   };
-
-  //One way to bind Counter Object to Handler
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
-  // 在对象的方法种，this指代对象, 在普通方法中，this指代windows, in strict mode, this is undefined
+  // 不能直接this.state.count++, 虽然数字增加了，但是react看不见
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 }); // overwrite
   };
 
   render() {
