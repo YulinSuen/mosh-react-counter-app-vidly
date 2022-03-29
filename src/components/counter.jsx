57df90a25174d620061abd4b8a449d29dbe6e8bb
Yulin <1710285545@qq.com>
2022-03-29 09:52:08 +0800

    11-Multiple Components in Sync

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value, // props包含了所有在Counters中为counter设置的属性
-  };
-  // 不能直接this.state.value++, 虽然数字增加了，但是react看不见
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 }); // overwrite
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         {/* 没有传入方法，而是传入引用 */}
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -33,12 +25,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    return this.state.value === 0 ? "Zero" : this.state.value;
+    return this.props.counter.value === 0 ? "Zero" : this.props.counter.value;
   }
 }
 
