d8492f46a24d1a8e63585d4f137240736bcae984
Yulin <1710285545@qq.com>
2022-03-29 11:39:32 +0800

    13 - Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,54 @@ import Counters from "./components/counters";
 import React, { Component } from "react";
 
 class App extends Component {
+  // state is local, props are read only
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
         <main className="Container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
   }
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,10 @@ class Navbar extends Component {
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {this.props.totalCounters}
+            </span>
           </a>
         </div>
       </nav>
