9bfb43dfcea394fc6a363afebbd6f8ee8ecb54a1
Yulin <1710285545@qq.com>
2022-03-29 11:49:58 +0800

    15- Lifecycle Hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 
 // Stateless Functional Component: sfc shortcut
-const Navbar = (props) => {
+// 太多props. , 解构destructure
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </a>
       </div>
