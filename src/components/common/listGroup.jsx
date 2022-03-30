62bae1cb67e0b7e0211d85d56949a19058661298
Yulin <1710285545@qq.com>
2022-03-30 15:40:54 +0800

    11- Filtering- Default Props

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-const ListGroup = () => {
-  return null;
+const ListGroup = (props) => {
+  const { items, textProperties, valueProperties } = props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperties]} className="list-group-item">
+          {item[textProperties]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
