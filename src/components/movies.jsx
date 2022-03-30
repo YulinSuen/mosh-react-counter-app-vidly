789c7b89ec3d7cbc772c685cdbfebeea6a91b698
Yulin <1710285545@qq.com>
2022-03-30 14:16:48 +0800

    6-Paginating Data

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,7 +4,7 @@ import Like from "./common/like";
 import Pagination from "./common/pagination";
 
 class Movies extends Component {
-  state = { movies: getMovies(), pageSize: 10 };
+  state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -20,11 +20,13 @@ class Movies extends Component {
   };
 
   handlePageChange = (page) => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
+
     if (count === 0) return <p>There are no movies in the database</p>;
     return (
       <React.Fragment>
@@ -67,7 +69,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
