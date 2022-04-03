35f3048ce09be6c9c8a89ce25aacad1db62d8306
Yulin <1710285545@qq.com>
2022-04-03 12:01:21 +0800

    8-Extracting a Reusable Input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   username = React.createRef();
 
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -13,7 +13,14 @@ class LoginForm extends Component {
     console.log("submitted");
   };
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -21,16 +28,24 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              autoFocus
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
