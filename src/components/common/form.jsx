cf25515efe1d1d2f3bd1d3ddb9843248aa6d442e
Yulin <1710285545@qq.com>
2022-04-06 16:12:29 +0800

    25-Exercise3 Search Box

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Input from "./input";
 import Joi from "joi-browser";
+import Select from "./select";
 
 class Form extends Component {
   state = {
@@ -60,6 +61,21 @@ class Form extends Component {
     return (
       <Input
         type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        options={options}
         name={name}
         value={data[name]}
         label={label}
